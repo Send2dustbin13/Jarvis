d6c43a6add5e45cdac838fbd49dc962855001247
Rsati_09 <138803796+Send2dustbin13@users.noreply.github.com>
2024-06-15 13:59:12 +0530

    Update Demo.py

--- Demo.py
+++ Demo.py
@@ -1,11 +1,23 @@
 import streamlit as st
-import pandas as pd
 
-data = {
-    'Fruit': ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'],
-    'Quantity': [10, 15, 20, 25, 30],
-    'Price': [0.5, 0.25, 0.75, 1.0, 2.0]
+from src.agstyler import PINLEFT, PRECISION_TWO, draw_grid
+
+formatter = {
+    'player_name': ('Player', PINLEFT),
+    'team': ('Team', {'width': 80}),
+    'poss': ('Posessions', {'width': 110}),
+    'mp': ('mp', {'width': 80}),
+    'raptor_total': ('RAPTOR', {**PRECISION_TWO, 'width': 100}),
+    'war_total': ('WAR', {**PRECISION_TWO, 'width': 80}),
+    'pace_impact': ('Pace Impact', {**PRECISION_TWO, 'width': 120})
 }
-df = pd.DataFrame(data)
 
-st.dataframe(df)
+row_number = st.number_input('Number of rows', min_value=0, value=20)
+data = draw_grid(
+    df.head(row_number),
+    formatter=formatter,
+    fit_columns=True,
+    selection='multiple',  # or 'single', or None
+    use_checkbox='True',  # or False by default
+    max_height=300
+)
